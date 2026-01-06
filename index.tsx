28fb9fb714904220e5cc4c55b0e06872f9c2fccc
Umair Afzal <umairispather@gmail.com>
2026-01-06 13:48:32 +0500

    fix index.tsx and use env API key

--- index.tsx
+++ index.tsx
@@ -1,15 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+// Mount the React app
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
